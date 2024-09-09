7af0e5781ce514159ac9f09eb1e3fa462989bcdc
Danilo Cassiano <93688833+danilocassiano@users.noreply.github.com>
2024-09-09 15:11:46 -0300

    feat(config): Criação  do projeto e configuração inicial.

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer">
+      <v-list-item title="Vue.js" subtitle="Lista de Tarefas"></v-list-item>
+      <v-divider></v-divider>
+      <v-list>
+        <v-list-item
+          v-for="(item, index) in itens"
+          :key="index"
+          :to="item.to"
+          link
+          class="d-flex align-center"
+        >
+          <v-icon left>{{ item.icon }}</v-icon>
+          <span class="ml-3">{{ item.title }}</span>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-app-bar-title>Application</v-app-bar-title>
+    </v-app-bar>
+
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    drawer: false,
+    itens: [
+      { title: "Tarefas", icon: "mdi-view-dashboard", to: '/'  },
+      { title: "sobre", icon: "mdi-help-box", to: '/sobre'  },
+    ],
+  }),
+}
+</script>
---
+++ src/router/index.ts
@@ -0,0 +1,39 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Tarefas',
+    component: () => import ('@/pages/Tarefas.vue')
+  },
+  {
+    path: '/sobre',
+    name: 'Sobre',
+    component: () => import('@/pages/Sobre.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+})
+
+router.onError((err, to) => {
+  if (err?.message?.includes('Failed to fetch dynamically imported module')) {
+    if (!localStorage.getItem('vuetify:dynamic-reload')) {
+      console.log('Reloading page to fix dynamic import error')
+      localStorage.setItem('vuetify:dynamic-reload', 'true')
+      location.assign(to.fullPath)
+    } else {
+      console.error('Dynamic import error, reloading page did not fix it', err)
+    }
+  } else {
+    console.error(err)
+  }
+})
+
+router.isReady().then(() => {
+  localStorage.removeItem('vuetify:dynamic-reload')
+})
+
+export default router
