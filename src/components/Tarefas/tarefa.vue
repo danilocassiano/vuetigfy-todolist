cf67232f78161bf57f5ea589ca277060ad7ae7e4
Danilo Cassiano <93688833+danilocassiano@users.noreply.github.com>
2024-09-09 20:10:39 -0300

    feat(tarefas): Criação do component terefas.

---
+++ src/components/Tarefas/tarefa.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <v-list lines="three" select-strategy="classic">
+      <v-list-item value="notifications">
+        <template v-slot:prepend="{ isActive }">
+          <v-list-item-action start>
+            <v-checkbox-btn :model-value="isActive"></v-checkbox-btn>
+          </v-list-item-action>
+        </template>
+        <v-list-item-title>Notifications</v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/pages/Tarefas.vue
+++ src/pages/Tarefas.vue
@@ -1,7 +1,23 @@
 <template>
-  <HelloWorld />
+  <div>
+    <v-list lines="three" select-strategy="classic">
+      <v-list-item value="notifications">
+        <tarefa />
+
+      </v-list-item>
+    </v-list>
+  </div>
 </template>
 
-<script lang="ts" setup>
-  //
+<script lang="ts">
+import tarefa from '../components/Tarefas/tarefa.vue';
+
+
+export default {
+  components: { tarefa },
+  name: 'Home',
+
+}
+
+//
 </script>
